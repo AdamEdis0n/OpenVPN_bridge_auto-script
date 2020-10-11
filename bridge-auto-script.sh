89cbe6443a6ace9006ef26fdbd8bd203b87ba8e8
AdamEdis0n <62962241+AdamEdis0n@users.noreply.github.com>
2020-10-11 21:42:03 +0200

    Update bridge-auto-script.sh

--- bridge-auto-script.sh
+++ bridge-auto-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Author: Adam Edison
 # Bridge
 br="br0"
 # TAP
@@ -32,3 +33,33 @@ declare -A subInterface25=(
 for t in $tap; do
     openvpn --mktun --dev $t 
 done 
+
+brctl addbr $br 
+
+declare -n preSubInterface 
+for preSubInterface in ${!subInterface@}; do
+        echo "Interface: ${preSubInterface[interface]}"
+        brctl addif $br ${preSubInterface[interface]} 
+done 
+
+for t in $tap; do
+    brctl addif $br $t
+    ifconfig $t 0.0.0.0 promisc up 
+done 
+
+declare -n preSubInterfaceIp 
+for preSubInterfaceIp in ${!subInterface@}; do
+        echo "Promisc up: ${preSubInterfaceIp[interface]}"
+        ifconfig ${preSubInterfaceIp[interface]} 0.0.0.0 promisc up 
+done 
+
+declare -n subInterfaceBrs 
+for subInterfaceBrs in ${!subInterface@}; do
+        echo "$br:${subInterfaceBrs[id]}"
+
+        if [ ${subInterfaceBrs[id]} == $firstId ]
+        then
+                ifconfig $br ${subInterfaceBrs[ip]} netmask ${subInterfaceBrs[netmask]} broadcast ${subInterfaceBrs[broadcast]}
+        else
+                ifconfig $br:${subInterfaceBrs[id]} ${subInterfaceBrs[ip]} netmask ${subInterfaceBrs[netmask]} broadcast ${subInterfaceBrs[broad$
+        fi done
