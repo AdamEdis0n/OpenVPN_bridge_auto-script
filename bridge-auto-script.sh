318c5f6c1426f45d737c829758bf801b7a17f09b
AdamEdis0n <62962241+AdamEdis0n@users.noreply.github.com>
2021-03-09 12:48:38 +0100

    Update bridge-auto-script.sh

--- bridge-auto-script.sh
+++ bridge-auto-script.sh
@@ -7,28 +7,28 @@ tap="tap0"
 # INTERFACES TO TAP
 firstId=21; declare -A subInterface21=(
         [id]='21'
-        [interface]='eth1.21'
-        [ip]='192.168.21.1'
-        [netmask]='255.255.255.224'
-        [broadcast]='192.168.21.31' ) 
+        [interface]='eth1.x'
+        [ip]='192.168.x.x'
+        [netmask]='255.255.255.x'
+        [broadcast]='192.168.x.x' ) 
 declare -A subInterface22=(
         [id]='22'
-        [interface]='eth1.22'
-        [ip]='192.168.22.1'
-        [netmask]='255.255.255.224'
-        [broadcast]='192.168.22.31' ) 
+        [interface]='eth1.x'
+        [ip]='192.168.x.x'
+        [netmask]='255.255.255.x'
+        [broadcast]='192.168.x.x' ) 
 declare -A subInterface23=(
         [id]='23'
-        [interface]='eth1.23'
-        [ip]='192.168.23.1'
-        [netmask]='255.255.255.224'
-        [broadcast]='192.168.23.31' ) 
+        [interface]='eth1.x'
+        [ip]='192.168.x.x'
+        [netmask]='255.255.255.x'
+        [broadcast]='192.168.x.x' ) 
 declare -A subInterface25=(
         [id]='25'
-        [interface]='eth1.25'
-        [ip]='192.168.25.1'
-        [netmask]='255.255.255.224'
-        [broadcast]='192.168.25.31' )
+        [interface]='eth1.x'
+        [ip]='192.168.x.x'
+        [netmask]='255.255.255.x'
+        [broadcast]='192.168.x.x' )
 
 for t in $tap; do
     openvpn --mktun --dev $t 
